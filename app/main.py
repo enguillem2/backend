a43333c10f51d1c8046823a3cbee98ebd0dd0f64
enguillem <enguillem@gmail.com>
2023-08-23 07:57:12 +0200

    canvis pq el cors funcionin

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI,HTTPException
 from routes.task import task
-
+from fastapi.middleware.cors import CORSMiddleware
+from decouple import config
 
 app = FastAPI()
 
+origins=[
+    config('FRONTED_URL')
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
